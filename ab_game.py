a63b847b2b997daad987d9b06d7d9520f0cef7e1
Takotail <19940202stephen@gmail.com>
2021-07-04 12:53:29 +0800

    Add new file

---
+++ ab_game.py
@@ -0,0 +1,38 @@
+#輸入數字
+
+print('輸入你的四個數字:')
+
+print('第一個個數字:')
+num_1 = (input(''))
+while str.isdigit(num_1) != True :
+ 	print('請輸入數字！：')
+ 	num_1 = (input(''))
+
+print('第二個個數字：')
+num_2 = (input(''))
+while str.isdigit(num_2) != True :
+ 	print('請輸入數字！：')
+ 	num_2 = (input(''))
+
+print('第三個個數字：')
+num_3 = (input(''))
+while str.isdigit(num_3) != True :
+ 	print('請輸入數字！：')
+ 	num_3 = (input(''))
+
+print('第三個個數字：')
+num_4 = (input(''))
+while str.isdigit(num_4) != True :
+ 	print('請輸入數字！：')
+ 	num_4 = (input(''))
+
+
+print(num_1, num_2, num_3, num_4)
+
+
+
+
+
+
+
+
