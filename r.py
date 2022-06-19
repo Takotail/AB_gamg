1135b5b9e4c9540e86bad7a070e61e57a728c548
Takotail <19940202stephen@gmail.com>
2022-06-19 16:05:21 +0800

    001

---
+++ r.py
@@ -0,0 +1,28 @@
+import random
+
+start = input('輸入起始值')
+start = int(start)
+end = input('輸入結束值')
+end = int(end)
+
+r = random.randint(start, end)
+count = 0
+
+while True:
+	num = input('猜猜數字')
+	num = int(num)
+	count += 1
+	if num == r:
+		print('猜中囉！')
+		print('這是你猜的第', count, '次')
+		break
+	elif num < r:
+		print('太小')
+	elif num > r:
+		print('太大')
+	print('這是你猜的第', count, '次')
+
+
+
+
+
